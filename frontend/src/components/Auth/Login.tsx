93a20b0f58948d276f39cf3063f6eb63dc93e330
meelan10 <milanwagle73@gmail.com>
2026-04-15 22:14:33 +0545

    .....

--- frontend/src/components/Auth/Login.tsx
+++ frontend/src/components/Auth/Login.tsx
@@ -1,11 +1,24 @@
+import { useState } from "react"
+
 const Login = () => {
-  const submitHandler = () => {
+   
+  useState
+
+
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    console.log("hello guys form submited")
 
   }
   return (
     <div className='flex h-screen w-screen items-center justify-center'>
     <div className='border-2 rounded-xl border-emerald-600 p-20'>
-      <form onSubmit={submitHandler}
+      <form onSubmit={(e)=>{
+        submitHandler(e)
+      }
+
+      }
       className='flex flex-col items-center justify-center'>
         <input required className=' bg-transparent outline-none border-2 border-emerald-600 py-4 px-5 text-xl rounded-full placeholder:text-white' type="email" placeholder='Enter your email' />
         <input required className='bg-transparent outline-none border-2 border-emerald-600 mt-3 py-4 px-5 text-xl rounded-full placeholder:text-white' type="password" placeholder='Enter password' />
