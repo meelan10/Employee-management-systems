ec0bfc8395658e8689e89b4d744115b6a054f5cd
meelan10 <milanwagle73@gmail.com>
2026-04-15 22:05:08 +0545

    function for onclick

--- frontend/src/components/Auth/Login.tsx
+++ frontend/src/components/Auth/Login.tsx
@@ -1,10 +1,14 @@
 const Login = () => {
+  const submitHandler = () => {
+
+  }
   return (
     <div className='flex h-screen w-screen items-center justify-center'>
     <div className='border-2 rounded-xl border-emerald-600 p-20'>
-      <form className='flex flex-col items-center justify-center'>
-        <input className=' bg-transparent outline-none border-2 border-emerald-600 py-4 px-5 text-xl rounded-full placeholder:text-grey-400' type="email" placeholder='Enter your email' />
-        <input  className='bg-transparent outline-none border-2 border-emerald-600 mt-3 py-4 px-5 text-xl rounded-full placeholder:text-white' type="password" placeholder='Enter password' />
+      <form onSubmit={submitHandler}
+      className='flex flex-col items-center justify-center'>
+        <input required className=' bg-transparent outline-none border-2 border-emerald-600 py-4 px-5 text-xl rounded-full placeholder:text-white' type="email" placeholder='Enter your email' />
+        <input required className='bg-transparent outline-none border-2 border-emerald-600 mt-3 py-4 px-5 text-xl rounded-full placeholder:text-white' type="password" placeholder='Enter password' />
         <button  className='text-white mt-5  outline-none border-none bg-emerald-600 py-4 px-5 text-xl rounded-full placeholder:text-white' >Login</button>
       </form>
     </div>
