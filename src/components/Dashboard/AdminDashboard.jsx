3cbe03a5363d30b9d669622244d3d9588e606c6c
meelan10 <milanwagle73@gmail.com>
2026-04-27 23:11:16 +0545

    another file created

--- src/components/Dashboard/AdminDashboard.jsx
+++ src/components/Dashboard/AdminDashboard.jsx
@@ -1,35 +1,14 @@
 import React from 'react'
 import Header from '../others/Header'
+import AllTask from '../others/AllTask'
+import CreateTask from '../others/CreateTask'
 
 const AdminDashboard = () => {
   return (
     <div className='h-screen w-full p-10'>
       <Header />
-
-      <div>
-        <form className='flex flex-wrap bg-red-300 w-full items-start justify-between text-white'>
-          <div className='w-1/2'> 
-            <div>  <h3 className='text-sm text-gray-300 mb-0.5'>Task Title</h3>
-          <input className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] bg-white text-black' type='text' placeholder='Make a UI design' />
-          </div> 
-          <div>  <h3 className='text-sm text-gray-300 mb-0.5'>Date</h3>
-          <input className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] text-black bg-white' type='date' />
-          </div>
-         <div> <h3 className='text-sm text-gray-300 mb-0.5 '>Assign to</h3>
-          <input className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] bg-white text-black' type='text' placeholder='employee name' />
-          </div>
-         <div>  <h3 className='text-sm text-gray-300 mb-0.5'>Category</h3>
-          <input className='text-sm py-1 px-2 w-4/5 rounded outline-none border-[1px] text-black bg-white' type='text' placeholder='design, dev, etc' />
-          </div>
-          </div>
-          <div className='w-1/2'>
-            <h3>Description</h3>
-          <textarea name='' id='' cols="30" rows="10" ></textarea>
-          </div>
-        
-          <button>Create Task</button>
-        </form>
-      </div>
+      <CreateTask />
+      <AllTask />
     </div>
   )
 }
---
+++ src/components/others/CreateTask.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const CreateTask = () => {
+  return (
+    <div>
+      <div>
+        <form className='flex flex-wrap bg-red-300 w-full items-start justify-between text-white'>
+          <div className='w-1/2'> 
+            <div>  <h3 className='text-sm text-gray-300 mb-0.5'>Task Title</h3>
+          <input className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] bg-white text-black' type='text' placeholder='Make a UI design' />
+          </div> 
+          <div>  <h3 className='text-sm text-gray-300 mb-0.5'>Date</h3>
+          <input className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] text-black bg-white' type='date' />
+          </div>
+         <div> <h3 className='text-sm text-gray-300 mb-0.5 '>Assign to</h3>
+          <input className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] bg-white text-black' type='text' placeholder='employee name' />
+          </div>
+         <div>  <h3 className='text-sm text-gray-300 mb-0.5'>Category</h3>
+          <input className='text-sm py-1 px-2 w-4/5 rounded outline-none border-[1px] text-black bg-white mb-4' type='text' placeholder='design, dev, etc' />
+          </div>
+          </div>
+          <div className='w-2/5 flex flex-col items-start'>
+            <h3 className='text-sm text-gray-300 mb-0.5'>Description</h3>
+          <textarea className='w-full h-44 bg-white text-sm text-black py-2 px-4 rounded outline-none gb-transparent border-[1px] border-gray-400' name='' id='' cols="30" rows="10" ></textarea>
+          </div>
+        
+          <button className='bg-emerald-500  py-3 hover:bg-emerald-600 px-5 rounded text-sm mt-4 w-full'>Create Task</button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default CreateTask
