cce1f9d245482e23b57c70acddfcf7d377139e18
meelan10 <milanwagle73@gmail.com>
2026-04-28 23:48:13 +0545

    value using localstorage

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,10 +1,18 @@
-import React, { createContext } from 'react'
+import React, { createContext, useEffect } from 'react'
+import { getLocalStorage } from '../utils/LocalStorage'
 
 export const AuthContext = createContext()
 
 const AuthProvider = ({ children }) => {
+
+  const [userData, setUser] = useState(null)
+
+ useEffect(()=> {
+    const { employees, admin} = getLocalStorage()
+  setUserData({employees,admin})
+ })
   return (
-    <AuthContext.Provider value={"Milan"}>
+    <AuthContext.Provider value={userData}>
       {children}
     </AuthContext.Provider>
   )
