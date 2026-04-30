8cd6bb5942da3b53d59a0831fead5d90a28ded88
meelan10 <milanwagle73@gmail.com>
2026-04-30 23:28:26 +0545

    login using password admin and user

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,30 @@
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
-import AdminDashboard from "./components/Dashboard/AdminDashboard"
-import { useEffect, useState } from "react"
-import Login from "./components/Auth/Login"
-import { setLocalStorage } from "./utils/LocalStorage"
-import AuthProvider from './context/AuthProvider'
+import AdminDashboard from './components/Dashboard/AdminDashboard'
+import { useState } from 'react'
+import Login from './components/Auth/Login'
+
 const App = () => {
-const [user, setuser] = useState(null)
+  const [user, setuser] = useState(null)
 
-
-const handleLogin = (email,password)=> {
-if(email == 'admin@me.com' && password == '123'){
-  setuser('admin')
-}
-else if (email == 'user@me.com ' && password == '123'){
-  setuser('employee')
-}
-else{
-  alert("Invalid Credentials")
-}
-}
- const data = useContext (AuthContext)
- console.log(data)
-
+  const handleLogin = (email, password) => {
+    if (email === 'admin@me.com' && password === '123') {
+      setuser('admin')
+    } else if (email === 'user@me.com' && password === '123') {
+      setuser('employee')
+    } else {
+      alert('Invalid Credentials')
+    }
+  }
 
   return (
     <>
-    {!user ? <Login handleLogin={handleLogin} />: '' }
-
-    {user === 'admin' ? <AdminDashboard /> : <EmployeeDashboard />}
+      {!user ? (
+        <Login handleLogin={handleLogin} />
+      ) : user === 'admin' ? (
+        <AdminDashboard />
+      ) : (
+        <EmployeeDashboard />
+      )}
     </>
   )
 }
--- src/utils/LocalStorage.jsx
+++ src/utils/LocalStorage.jsx
@@ -197,11 +197,9 @@ export const  setLocalStorage=() => {
     localStorage.setItem('admin',JSON.stringify(admin))
 }
 
-export const  getLocalStorage=() => {
+export const getLocalStorage = () => {
   const employees = JSON.parse(localStorage.getItem('employees'))
-   const admin = JSON.parse(localStorage.getItem('admin'))
+  const admin = JSON.parse(localStorage.getItem('admin'))
 
-  return{employees}
-
-  
+  return { employees, admin }
 }
